be2d2cdac456d1614086144852ad04d80da1e3b5
rayniel95 <rainyel.ramos@gmail.com>
2023-12-24 16:11:35 -0500

    create basic layout

--- erc20-indexer/src/app/page.tsx
+++ erc20-indexer/src/app/page.tsx
@@ -1,12 +1,28 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { Col, Container, Row } from "react-bootstrap";
+import AssetsList from "./components/AssetsList";
 
 
 export default function Home() {
   return (
     <main>
-      <h1>Erc20 Indexer</h1>
-      <ConnectButton />
-
+      <Container>
+        <Row className="mt-3">
+          <Col>
+            <h1 className="text-center">Erc20 Indexer</h1>
+          </Col>
+          <Col sm={2}> 
+            {/* TODO - move this connect button to the end */}
+            <ConnectButton />
+          </Col>
+        </Row>
+        <hr />
+        <Row>
+          <Col>
+            <AssetsList/>
+          </Col>
+        </Row>
+      </Container>
     </main>
   )
 }
