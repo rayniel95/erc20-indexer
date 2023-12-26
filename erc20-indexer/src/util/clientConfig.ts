f1df3107a7e3a596db2b2e4aa1be0f93a58bc4af
rayniel95 <rainyel.ramos@gmail.com>
2023-12-26 17:42:46 -0500

    delete print

--- erc20-indexer/src/util/clientConfig.ts
+++ erc20-indexer/src/util/clientConfig.ts
@@ -9,7 +9,6 @@ export function getAlchemyClient(chain: any) {
         apiKey: process.env.NEXT_PUBLIC_ETHEREUM_SEPOLIA_API_KEY!,
         network: Network.ETH_SEPOLIA,
     };
-    console.log(process.env.NEXT_PUBLIC_ETHEREUM_SEPOLIA_API_KEY)
     if (chain?.name === mainnet.name) {
         config = {
             apiKey: process.env.NEXT_PUBLIC_ETHEREUM_MAINNET_API_KEY!,
