bcd9cb3c57fa4a4cd0176589442cc8fc574013bf
rayniel95 <rainyel.ramos@gmail.com>
2023-12-26 17:43:52 -0500

    add typing

--- erc20-indexer/src/app/components/AssetsList.tsx
+++ erc20-indexer/src/app/components/AssetsList.tsx
@@ -1,28 +1,32 @@
 'use client'
 
-import { useState, useCallback } from "react";
-import { AssetTransfersCategory, TokenBalanceType } from "alchemy-sdk";
+import { useCallback, useState } from "react";
+import { TokenBalance, TokenBalanceType, TokenMetadataResponse } from "alchemy-sdk";
 import InfiniteScroll from "react-infinite-scroller";
-import { Alchemy, Network, Utils } from 'alchemy-sdk';
 import { Spinner } from "react-bootstrap";
-import { useAccount, useNetwork, usePublicClient } from "wagmi";
 import { getAlchemyClient, NetworkUrls } from "@/util/clientConfig";
 import ERC20Token from "./ERC20Token";
 import { getTokenMetadataBatch } from "@/util/batchTokenMetadata";
 
+export interface IAssetsListProps {
+  chain: any;
+  address: string;
+}
 
-export default function AssetsList() {
+type TokenResponse = TokenMetadataResponse & TokenBalance
+
+export default function AssetsList({ chain, address }: IAssetsListProps) {
   //TODO - maybe turn this two states into memoized vars
-  const [items, setItems] = useState<any[]>([]);
-  const [pageKey, setPageKey] = useState<string | undefined>(undefined)
-  const { chain} = useNetwork()
-  const { address } = useAccount()
+  // const [items, setItems] = useState<TokenResponse[]>([]);
+  // const [pageKey, setPageKey] = useState<string | undefined>(undefined)
+  let items: TokenResponse[] = []
+  let pageKey: string | undefined = undefined
 
   const hasMore = (pageKey == undefined && items.length == 0) || (pageKey != undefined && items.length > 0)
 
   const alchemy = getAlchemyClient(chain)
 
-  async function fetchData() {
+  const fetchData = useCallback(async function()  {
     try {
       const data = await alchemy.core.getTokenBalances(
         address as string,
@@ -36,13 +40,16 @@ export default function AssetsList() {
         NetworkUrls[alchemy.config.network],
         alchemy.config.apiKey
       )
-      
-      setItems(items.concat(data.tokenBalances.map((item, index) => ({ ...item, ...tokenMetadata[index] }))))
-      setPageKey(data.pageKey)
+      console.log(items)
+      const newItems = items.concat(data.tokenBalances.map((item, index) => ({ ...item, ...tokenMetadata[index].result })))
+      // setItems(newItems)
+      // setPageKey(data.pageKey)
+      items = newItems
+      pageKey = data.pageKey
     } catch (error) {
       console.error('Error:', error);
     }
-  }
+  }, [items, pageKey])
 
   return (
     <div style={{ height: "700px", overflow: "auto" }}>
@@ -56,10 +63,10 @@ export default function AssetsList() {
         useWindow={false}
       >
         {
-          items.map(
-            (item) => (
-              <div key={item.contractAddress}>
-                <ERC20Token symbol={item.symbol} balance={item.balance} logo={item.logo} decimals={item.decimals} />
+          items.length > 0 && items.map(
+            (item, index) => (
+              <div key={index}>
+                <ERC20Token symbol={item.symbol!} balance={item.tokenBalance!} logo={item.logo!} decimals={item.decimals!} />
                 <hr />
               </div>
             )
@@ -69,126 +76,3 @@ export default function AssetsList() {
     </div>
   );
 }
-
-/*
-import {
-  Box,
-  Button,
-  Center,
-  Flex,
-  Heading,
-  Image,
-  Input,
-  SimpleGrid,
-  Text,
-} from '@chakra-ui/react';
-import { Alchemy, Network, Utils } from 'alchemy-sdk';
-import { useState } from 'react';
-
-function App() {
-  const [userAddress, setUserAddress] = useState('');
-  const [results, setResults] = useState([]);
-  const [hasQueried, setHasQueried] = useState(false);
-  const [tokenDataObjects, setTokenDataObjects] = useState([]);
-
-  async function getTokenBalance() {
-    const config = {
-      apiKey: '<-- COPY-PASTE YOUR ALCHEMY API KEY HERE -->',
-      network: Network.ETH_MAINNET,
-    };
-
-    const alchemy = new Alchemy(config);
-    const data = await alchemy.core.getTokenBalances(userAddress);
-
-    setResults(data);
-
-    const tokenDataPromises = [];
-
-    for (let i = 0; i < data.tokenBalances.length; i++) {
-      const tokenData = alchemy.core.getTokenMetadata(
-        data.tokenBalances[i].contractAddress
-      );
-      tokenDataPromises.push(tokenData);
-    }
-
-    setTokenDataObjects(await Promise.all(tokenDataPromises));
-    setHasQueried(true);
-  }
-  return (
-    <Box w="100vw">
-      <Center>
-        <Flex
-          alignItems={'center'}
-          justifyContent="center"
-          flexDirection={'column'}
-        >
-          <Heading mb={0} fontSize={36}>
-            ERC-20 Token Indexer
-          </Heading>
-          <Text>
-            Plug in an address and this website will return all of its ERC-20
-            token balances!
-          </Text>
-        </Flex>
-      </Center>
-      <Flex
-        w="100%"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent={'center'}
-      >
-        <Heading mt={42}>
-          Get all the ERC-20 token balances of this address:
-        </Heading>
-        <Input
-          onChange={(e) => setUserAddress(e.target.value)}
-          color="black"
-          w="600px"
-          textAlign="center"
-          p={4}
-          bgColor="white"
-          fontSize={24}
-        />
-        <Button fontSize={20} onClick={getTokenBalance} mt={36} bgColor="blue">
-          Check ERC-20 Token Balances
-        </Button>
-
-        <Heading my={36}>ERC-20 token balances:</Heading>
-
-        {hasQueried ? (
-          <SimpleGrid w={'90vw'} columns={4} spacing={24}>
-            {results.tokenBalances.map((e, i) => {
-              return (
-                <Flex
-                  flexDir={'column'}
-                  color="white"
-                  bg="blue"
-                  w={'20vw'}
-                  key={e.id}
-                >
-                  <Box>
-                    <b>Symbol:</b> ${tokenDataObjects[i].symbol}&nbsp;
-                  </Box>
-                  <Box>
-                    <b>Balance:</b>&nbsp;
-                    {Utils.formatUnits(
-                      e.tokenBalance,
-                      tokenDataObjects[i].decimals
-                    )}
-                  </Box>
-                  <Image src={tokenDataObjects[i].logo} />
-                </Flex>
-              );
-            })}
-          </SimpleGrid>
-        ) : (
-          'Please make a query! This may take a few seconds...'
-        )}
-      </Flex>
-    </Box>
-  );
-}
-
-export default App;
-
-*/
