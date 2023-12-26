4daf6b311a1d4e1facf4c90a06031c8d2c7f1606
rayniel95 <rainyel.ramos@gmail.com>
2023-12-26 17:43:25 -0500

    create wrapping comp

--- erc20-indexer/src/app/page.tsx
+++ erc20-indexer/src/app/page.tsx
@@ -2,8 +2,8 @@
 
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Col, Container, Row } from "react-bootstrap";
-import AssetsList from "./components/AssetsList";
 import { useAccount } from "wagmi";
+import WalletConnect from "./components/WalletConnect";
 
 
 export default function Home() {
@@ -15,16 +15,16 @@ export default function Home() {
         <Row className="mt-3">
           <Col>
             <h1 className="text-center">Erc20 Indexer</h1>
+            <hr />
           </Col>
           <Col sm={2}> 
             {/* TODO - move this connect button to the end */}
             <ConnectButton />
           </Col>
         </Row>
-        <hr />
         <Row>
           <Col>
-            {isConnected && <AssetsList/>}
+            {isConnected && <WalletConnect/>}
           </Col>
         </Row>
       </Container>
