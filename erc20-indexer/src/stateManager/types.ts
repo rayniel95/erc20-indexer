924e65133f4e36583de33134de41e8989e1a5bc7
rayniel95 <rainyel.ramos@gmail.com>
2023-12-27 16:50:28 -0500

    delete not necessary code

--- erc20-indexer/src/stateManager/types.ts
+++
@@ -1,7 +0,0 @@
-
-export interface INetworkManager{
-    getBlocks(start: number, end: number): Promise<Array<any>>
-    getLatestTransactions(start: number, end: number): Promise<Array<any>>
-    getBlocksFromNegativeIndex(start: number, end: number): Promise<Array<any>>
-    getLatestBlocks(cantity: number): Promise<Array<any>>
-}
